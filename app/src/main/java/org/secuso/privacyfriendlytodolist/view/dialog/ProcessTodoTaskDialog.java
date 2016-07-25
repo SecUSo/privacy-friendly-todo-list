2f319e02dfb6f54501ec6bb6be5a6d9f7079bb67
Simon Breitfelder <sbreitf1@web.de>
2016-07-26 00:23:23 +0200

    Minor fixes

--- app/src/main/java/org/secuso/privacyfriendlytodolist/view/dialog/ProcessTodoTaskDialog.java
+++ app/src/main/java/org/secuso/privacyfriendlytodolist/view/dialog/ProcessTodoTaskDialog.java
@@ -56,6 +56,7 @@ public class ProcessTodoTaskDialog extends FullScreenDialog {
         taskName.setText(task.getName());
         taskDescription.setText(task.getDescription());
         prioritySelector.setText(Helper.priority2String(context, task.getPriority()));
+        taskPriority = task.getPriority();
         progressSelector.setProgress(task.getProgress());
         if(task.getDeadline() <= 0)
             deadlineTextView.setText(context.getString(R.string.no_deadline));
