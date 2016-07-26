63a9752e9180cc3311cab14356a5339b57200a57
Dominik Puellen <dominik@puellen.org>
2016-07-26 19:10:02 +0200

    fixed about activity

--- app/src/main/java/org/secuso/privacyfriendlytodolist/view/AboutActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlytodolist/view/AboutActivity.java
@@ -1,9 +1,13 @@
 package org.secuso.privacyfriendlytodolist.view;
 
 import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import org.secuso.privacyfriendlytodolist.R;
@@ -16,9 +20,23 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
 
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle(R.string.menu_about);
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#024265")));
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_about);
+
+        if (toolbar != null) {
+            toolbar.setTitle(R.string.menu_about);
+            toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white));
+            setSupportActionBar(toolbar);
+
+        }
+
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            final Drawable upArrow = ContextCompat.getDrawable(this, R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+            upArrow.setColorFilter(ContextCompat.getColor(this, R.color.white), PorterDuff.Mode.SRC_ATOP);
+            getSupportActionBar().setHomeAsUpIndicator(upArrow);
+        }
     }
 
     @Override
