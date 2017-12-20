7f170816fb9c836e016db9cd0fef7973dfa211dd
Naitbobbes <33450097+Naitbobbes@users.noreply.github.com>
2017-12-20 16:40:53 +0100

    new Class for recycler

---
+++ app/src/main/java/org/secuso/privacyfriendlytodolist/view/dialog/RecyclerActivity.java
@@ -0,0 +1,11 @@
+package org.secuso.privacyfriendlytodolist.view.dialog;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sebbi on 20.12.2017.
+ */
+
+public class RecyclerActivity extends AppCompatActivity {
+    
+}
