db1795da2ee7283cd4607620b0e8275bce62931b
Naitbobbes <33450097+Naitbobbes@users.noreply.github.com>
2017-12-20 16:55:56 +0100

    activity_recycle view added

--- app/src/main/java/org/secuso/privacyfriendlytodolist/view/RecyclerActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlytodolist/view/RecyclerActivity.java
@@ -1,5 +1,7 @@
 package org.secuso.privacyfriendlytodolist.view;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,5 +9,13 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class RecyclerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
 
 }
