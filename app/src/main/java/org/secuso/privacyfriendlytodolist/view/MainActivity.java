eaad8a7f0d6eaaecd06d6467c1e3b65da721c442
Naitbobbes <33450097+Naitbobbes@users.noreply.github.com>
2018-01-09 23:46:45 +0100

    fixed items in navigation

--- app/src/main/java/org/secuso/privacyfriendlytodolist/view/MainActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlytodolist/view/MainActivity.java
@@ -88,6 +88,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private static final long UnlockPeriod = 30000; // keep the app unlocked for 30 seconds after switching to another activity (settings/help/about)
 
 
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
