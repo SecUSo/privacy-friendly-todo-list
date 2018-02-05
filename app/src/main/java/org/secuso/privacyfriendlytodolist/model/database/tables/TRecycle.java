823a5b5b340829b86dfad33ef74968252aee955d
Naitbobbes <33450097+Naitbobbes@users.noreply.github.com>
2018-02-05 12:36:02 +0100

    licenses added to classes

--- app/src/main/java/org/secuso/privacyfriendlytodolist/model/database/tables/TRecycle.java
+++
@@ -1,8 +0,0 @@
-package org.secuso.privacyfriendlytodolist.model.database.tables;
-
-/**
- * Created by sebbi on 22.12.2017.
- */
-
-public class TRecycle {
-}
--- app/src/main/java/org/secuso/privacyfriendlytodolist/tutorial/TutorialActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlytodolist/tutorial/TutorialActivity.java
@@ -1,3 +1,20 @@
+/*
+ This file is part of Privacy Friendly To-Do List.
+
+ Privacy Friendly To-Do List is free software:
+ you can redistribute it and/or modify it under the terms of the
+ GNU General Public License as published by the Free Software Foundation,
+ either version 3 of the License, or any later version.
+
+ Privacy Friendly To-Do List is distributed in the hope
+ that it will be useful, but WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Privacy Friendly To-Do List. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.secuso.privacyfriendlytodolist.tutorial;
 
 
@@ -25,8 +42,8 @@ import org.secuso.privacyfriendlytodolist.view.MainActivity;
 
 /**
  *
- * @author sebbi
- * @version 20171206
+ * @author Sebastian Lutz
+ * @version 06.12.2017
  *
  */
 
