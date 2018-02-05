2c649375e5ee87adeb163a4c095ff66a2e318799
Naitbobbes <33450097+Naitbobbes@users.noreply.github.com>
2018-02-05 13:10:30 +0100

    exemplary snackbar added to clear-bin

--- app/src/main/java/org/secuso/privacyfriendlytodolist/view/RecyclerActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlytodolist/view/RecyclerActivity.java
@@ -17,10 +17,12 @@
 
 package org.secuso.privacyfriendlytodolist.view;
 
+import android.content.ClipData;
 import android.content.Context;
 import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -74,13 +76,16 @@ public class RecyclerActivity extends AppCompatActivity{
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
+
         switch (item.getItemId()) {
             // Respond to the action bar's Up/Home button
             case android.R.id.home:
                 finish();
                 return true;
             case R.id.btn_clear:
-                Toast.makeText(this, R.string.toast_del, Toast.LENGTH_SHORT).show();
+                Snackbar snackbar = Snackbar.make(this.findViewById(R.id.btn_clear), "Hello World", Snackbar.LENGTH_LONG);
+                snackbar.show();
+                //Toast.makeText(this, R.string.toast_del, Toast.LENGTH_SHORT).show();
                 dbhelper = DatabaseHelper.getInstance(this);
                 ArrayList<TodoTask> tasks;
                 tasks = DBQueryHandler.getBin(dbhelper.getReadableDatabase());
