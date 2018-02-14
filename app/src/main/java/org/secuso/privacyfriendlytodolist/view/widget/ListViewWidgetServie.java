931098c7d9b564d9c6df6ba29adc6d4d91f3ce1c
Naitbobbes <33450097+Naitbobbes@users.noreply.github.com>
2018-02-14 01:57:18 +0100

    licence and javadoc update

--- app/src/main/java/org/secuso/privacyfriendlytodolist/view/widget/ListViewWidgetServie.java
+++ app/src/main/java/org/secuso/privacyfriendlytodolist/view/widget/ListViewWidgetServie.java
@@ -1,3 +1,20 @@
+/*
+ This file is part of Privacy Friendly To-Do List.
+
+ Privacy Friendly To-Do List is free software:
+ you can redistribute it and/or modify it under the terms of the
+ GNU General Public License as published by the Free Software Foundation,
+ either version 3 of the License, or any later version.
+
+ Privacy Friendly To-Do List is distributed in the hope
+ that it will be useful, but WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Privacy Friendly To-Do List. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.secuso.privacyfriendlytodolist.view.widget;
 
 import android.content.Context;
@@ -15,7 +32,10 @@ import org.secuso.privacyfriendlytodolist.model.database.DatabaseHelper;
 import java.util.ArrayList;
 
 /**
- * Created by sebbi on 14.02.2018.
+ * Created by Sebastian Lutz on 14.02.2018.
+ *
+ * Service needed to give the widget its data.
+ *
  */
 
 public class ListViewWidgetServie extends RemoteViewsService {
