519e155891ff444600ebf1cc128ef854847d77b6
Christopher Beckmann <chris.beckm@gmail.com>
2018-04-04 13:47:14 +0200

    Fixed #42. Update needs to be pushed to users.

--- app/src/main/assets/from_1_to_2.sql
+++ app/src/main/assets/from_1_to_2.sql
@@ -1,4 +1,17 @@
-ALTER TABLE todo_list DROP COLUMN description;
+-- drop column description from todo_list
+PRAGMA foreign_keys=off;
+BEGIN TRANSACTION;
+ALTER TABLE todo_list RENAME TO temp_table;
+CREATE TABLE todo_list (
+	_id	INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT NOT NULL
+);
+INSERT INTO todo_list (_id, name)
+  SELECT _id, name
+  FROM temp_table;
+DROP TABLE temp_table;
+COMMIT;
+PRAGMA foreign_keys=on;
 
 ALTER TABLE todo_task ADD in_trash INTEGER NOT NULL DEFAULT 0;
 
