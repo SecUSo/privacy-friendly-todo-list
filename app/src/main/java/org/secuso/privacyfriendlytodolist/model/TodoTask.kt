SecUSo/privacy-friendly-todo-list

commit 8b48ed709723648f036225fe05481e0200f605cd

Added TaskChangeObserver that triggers update of alarm for next due task. With that it was possible to remove the many triggers for that spread over the whole app. Giving details to model observer: How many lists, tasks, subtasks did change. Removed toast message when due task alarm was set. It came too often.

app/src/main/java/org/secuso/privacyfriendlytodolist/view/ExpandableTodoTaskAdapter.kt
@@ -481,12 +481,11 @@ class ExpandableTodoTaskAdapter(private val context: Context, private val model:
                             currentTask.setAllSubtasksDone(inverted)
                             currentTask.getProgress(hasAutoProgress())
                             currentTask.setChanged()
-                            notifyDataSetChanged()
                             for (subtask: TodoSubtask in currentTask.getSubtasks()) {
                                 subtask.setDone(inverted)
                             }
                             model.saveTodoTaskAndSubtasksInDb(currentTask) {
-                                JobManager.startUpdateAlarmJob(context)
+                                notifyDataSetChanged()
                             }
                         }
                         snackBar.show()
@@ -494,13 +493,11 @@ class ExpandableTodoTaskAdapter(private val context: Context, private val model:
                         currentTask.setAllSubtasksDone(buttonView.isChecked)
                         currentTask.getProgress(hasAutoProgress())
                         currentTask.setChanged()
-                        notifyDataSetChanged()
                         for (subtask: TodoSubtask in currentTask.getSubtasks()) {
                             subtask.setChanged()
-                            notifyDataSetChanged()
                         }
                         model.saveTodoTaskInDb(currentTask) {
-                            JobManager.startUpdateAlarmJob(context)
+                            notifyDataSetChanged()
                         }
                     }
                 }
@@ -612,16 +609,16 @@ class ExpandableTodoTaskAdapter(private val context: Context, private val model:
                 svh.done.setOnCheckedChangeListener { buttonView, isChecked ->
                     if (buttonView.isPressed) {
                         currentSubtask.setDone(buttonView.isChecked)
-                        // check if entire task is done now (when all subtasks are done)
-                        val doneStatusChanged = currentTask.doneStatusChanged()
                         currentSubtask.setChanged()
-                        model.saveTodoSubtaskInDb(currentSubtask) { counter1: Int? ->
-                            currentTask.getProgress(hasAutoProgress())
-                            model.saveTodoTaskInDb(currentTask) {
-                                counter2: Int? -> notifyDataSetChanged()
-                                if (doneStatusChanged) {
-                                    JobManager.startUpdateAlarmJob(context)
+                        model.saveTodoSubtaskInDb(currentSubtask) {
+                            if (hasAutoProgress()) {
+                                // If having auto-progress, update the progress and save it.
+                                currentTask.getProgress(true)
+                                model.saveTodoTaskInDb(currentTask) {
+                                    notifyDataSetChanged()
                                 }
+                            } else {
+                                notifyDataSetChanged()
                             }
                         }
                     }

app/src/main/java/org/secuso/privacyfriendlytodolist/PFAApplication.kt
@@ -23,6 +23,8 @@ import androidx.work.Configuration
 import org.secuso.privacyfriendlybackup.api.pfa.BackupManager
 import org.secuso.privacyfriendlytodolist.backup.BackupCreator
 import org.secuso.privacyfriendlytodolist.backup.BackupRestorer
+import org.secuso.privacyfriendlytodolist.model.Model
+import org.secuso.privacyfriendlytodolist.observer.TaskChangeObserver
 import org.secuso.privacyfriendlytodolist.service.JobManager
 import org.secuso.privacyfriendlytodolist.view.widget.TodoListWidget
 
@@ -38,5 +40,6 @@ class PFAApplication : Application(), Configuration.Provider {
         // When the application exits, its alarms get cancelled by the OS.
         // So ensure here that an alarm is set for the next due task:
         JobManager.startUpdateAlarmJob(this)
+        Model.registerModelObserver(TaskChangeObserver)
     }
 }

app/src/main/java/org/secuso/privacyfriendlytodolist/model/Model.kt
@@ -67,15 +67,19 @@ object Model {
         modelObservers.remove(modelObserver)
     }
 
-    fun notifyDataChanged(context: Context) {
-        for (modelObserver in modelObservers) {
-            modelObserver.onTodoDataChanged(context)
+    fun notifyDataChanged(context: Context, changedLists: Int, changedTasks: Int, changedSubtasks: Int) {
+        if (changedLists > 0 || changedTasks > 0 || changedSubtasks > 0) {
+            for (modelObserver in modelObservers) {
+                modelObserver.onTodoDataChanged(context, changedLists, changedTasks, changedSubtasks)
+            }
         }
     }
 
-    fun notifyDataChangedFromOutside(context: Context) {
-        for (modelObserver in modelObservers) {
-            modelObserver.onTodoDataChangedFromOutside(context)
+    fun notifyDataChangedFromOutside(context: Context, changedLists: Int, changedTasks: Int, changedSubtasks: Int) {
+        if (changedLists > 0 || changedTasks > 0 || changedSubtasks > 0) {
+            for (modelObserver in modelObservers) {
+                modelObserver.onTodoDataChangedFromOutside(context, changedLists, changedTasks, changedSubtasks)
+            }
         }
     }
 }

app/src/main/java/org/secuso/privacyfriendlytodolist/model/ModelObserver.kt
@@ -23,9 +23,9 @@ interface ModelObserver {
     /**
      * Gets called when a the data in the data model (lists, tasks, subtasks) gets changed.
      *
-     * For example if a task gets changed and saved.
+     * For example if a task gets changed by the user and saved.
      */
-    fun onTodoDataChanged(context: Context) {
+    fun onTodoDataChanged(context: Context, changedLists: Int, changedTasks: Int, changedSubtasks: Int) {
     }
 
     /**
@@ -34,6 +34,6 @@ interface ModelObserver {
      *
      * For example if a task gets changed and saved by a reminder notification action.
      */
-    fun onTodoDataChangedFromOutside(context: Context) {
+    fun onTodoDataChangedFromOutside(context: Context, changedLists: Int, changedTasks: Int, changedSubtasks: Int) {
     }
 }

app/src/main/java/org/secuso/privacyfriendlytodolist/model/ModelServices.kt
@@ -90,7 +90,7 @@ class ModelServices(
         return coroutineScope.launch(Dispatchers.IO) {
             val todoTask = services.getNextDueTask(now)
             dispatchResult(deliveryOption, resultConsumer, todoTask.left)
-            notifyDataChanged(todoTask.right)
+            notifyDataChanged(0, todoTask.right, 0)
         }
     }
 
@@ -111,21 +111,21 @@ class ModelServices(
 
     fun deleteTodoList(todoListId: Int,
                        deliveryOption: DeliveryOption = DeliveryOption.POST,
-                       resultConsumer: ResultConsumer<Int>? = null): Job {
+                       resultConsumer: ResultConsumer<Triple<Int, Int, Int>>? = null): Job {
         return coroutineScope.launch(Dispatchers.IO) {
             val counter = services.deleteTodoList(todoListId)
             dispatchResult(deliveryOption, resultConsumer, counter)
-            notifyDataChanged(counter)
+            notifyDataChanged(counter.first, counter.second, counter.third)
         }
     }
 
     fun deleteTodoTask(todoTask: TodoTask,
                        deliveryOption: DeliveryOption = DeliveryOption.POST,
-                       resultConsumer: ResultConsumer<Int>? = null): Job {
+                       resultConsumer: ResultConsumer<Tuple<Int, Int>>? = null): Job {
         return coroutineScope.launch(Dispatchers.IO) {
-            val counter = services.deleteTodoTask(todoTask)
+            val counter = services.deleteTodoTaskAndSubtasks(todoTask)
             dispatchResult(deliveryOption, resultConsumer, counter)
-            notifyDataChanged(counter)
+            notifyDataChanged(0, counter.left, counter.right)
         }
     }
 
@@ -135,18 +135,18 @@ class ModelServices(
         return coroutineScope.launch(Dispatchers.IO) {
             val counter = services.deleteTodoSubtask(subtask)
             dispatchResult(deliveryOption, resultConsumer, counter)
-            notifyDataChanged(counter)
+            notifyDataChanged(0, 0, counter)
         }
     }
 
     fun setTaskAndSubtasksInRecycleBin(todoTask: TodoTask,
                                        inRecycleBin: Boolean,
                                        deliveryOption: DeliveryOption = DeliveryOption.POST,
-                                       resultConsumer: ResultConsumer<Int>? = null): Job {
+                                       resultConsumer: ResultConsumer<Tuple<Int, Int>>? = null): Job {
         return coroutineScope.launch(Dispatchers.IO) {
             val counter = services.setTaskAndSubtasksInRecycleBin(todoTask, inRecycleBin)
             dispatchResult(deliveryOption, resultConsumer, counter)
-            notifyDataChanged(counter)
+            notifyDataChanged(0, counter.left, counter.right)
         }
     }
 
@@ -157,7 +157,7 @@ class ModelServices(
         return coroutineScope.launch(Dispatchers.IO) {
             val counter = services.setSubtaskInRecycleBin(subtask, inRecycleBin)
             dispatchResult(deliveryOption, resultConsumer, counter)
-            notifyDataChanged(counter)
+            notifyDataChanged(0, 0, counter)
         }
     }
 
@@ -198,11 +198,11 @@ class ModelServices(
     }
 
     fun clearRecycleBin(deliveryOption: DeliveryOption = DeliveryOption.POST,
-                        resultConsumer: ResultConsumer<Int>? = null): Job {
+                        resultConsumer: ResultConsumer<Tuple<Int, Int>>? = null): Job {
         return coroutineScope.launch(Dispatchers.IO) {
             val counter = services.clearRecycleBin()
             dispatchResult(deliveryOption, resultConsumer, counter)
-            notifyDataChanged(counter)
+            notifyDataChanged(0, counter.left, counter.right)
         }
     }
 
@@ -246,7 +246,7 @@ class ModelServices(
         return coroutineScope.launch(Dispatchers.IO) {
             val counter = services.saveTodoListInDb(todoList)
             dispatchResult(deliveryOption, resultConsumer, counter)
-            notifyDataChanged(counter)
+            notifyDataChanged(counter, 0, 0)
         }
     }
 
@@ -256,17 +256,17 @@ class ModelServices(
         return coroutineScope.launch(Dispatchers.IO) {
             val counter = services.saveTodoTaskInDb(todoTask)
             dispatchResult(deliveryOption, resultConsumer, counter)
-            notifyDataChanged(counter)
+            notifyDataChanged(0, counter, 0)
         }
     }
 
     fun saveTodoTaskAndSubtasksInDb(todoTask: TodoTask,
                                     deliveryOption: DeliveryOption = DeliveryOption.POST,
-                                    resultConsumer: ResultConsumer<Int>? = null): Job {
+                                    resultConsumer: ResultConsumer<Tuple<Int, Int>>? = null): Job {
         return coroutineScope.launch(Dispatchers.IO) {
             val counter = services.saveTodoTaskAndSubtasksInDb(todoTask)
             dispatchResult(deliveryOption, resultConsumer, counter)
-            notifyDataChanged(counter)
+            notifyDataChanged(0, counter.left, counter.right)
         }
     }
 
@@ -276,7 +276,7 @@ class ModelServices(
         return coroutineScope.launch(Dispatchers.IO) {
             val counter = services.saveTodoSubtaskInDb(todoSubtask)
             dispatchResult(deliveryOption, resultConsumer, counter)
-            notifyDataChanged(counter)
+            notifyDataChanged(0, 0, counter)
         }
     }
 
@@ -286,7 +286,7 @@ class ModelServices(
         return coroutineScope.launch(Dispatchers.IO) {
             val counter = services.saveTodoListsSortOrderInDb(todoListIds)
             dispatchResult(deliveryOption, resultConsumer, counter)
-            notifyDataChanged(counter)
+            notifyDataChanged(counter, 0, 0)
         }
     }
 
@@ -296,7 +296,7 @@ class ModelServices(
         return coroutineScope.launch(Dispatchers.IO) {
             val counter = services.saveTodoTasksSortOrderInDb(todoTasks)
             dispatchResult(deliveryOption, resultConsumer, counter)
-            notifyDataChanged(counter)
+            notifyDataChanged(0, counter, 0)
         }
     }
 
@@ -306,16 +306,16 @@ class ModelServices(
         return coroutineScope.launch(Dispatchers.IO) {
             val counter = services.saveTodoSubtasksSortOrderInDb(todoSubtasks)
             dispatchResult(deliveryOption, resultConsumer, counter)
-            notifyDataChanged(counter)
+            notifyDataChanged(0, 0, counter)
         }
     }
 
     fun deleteAllData(deliveryOption: DeliveryOption = DeliveryOption.POST,
-                      resultConsumer: ResultConsumer<Int>? = null): Job {
+                      resultConsumer: ResultConsumer<Triple<Int, Int, Int>>? = null): Job {
         return coroutineScope.launch(Dispatchers.IO) {
             val counter = services.deleteAllData()
             dispatchResult(deliveryOption, resultConsumer, counter)
-            notifyDataChanged(counter)
+            notifyDataChanged(counter.first, counter.second, counter.third)
         }
     }
 
@@ -334,7 +334,8 @@ class ModelServices(
         return coroutineScope.launch(Dispatchers.IO) {
             val result = services.importCSVData(deleteAllDataBeforeImport, csvDataUri)
             dispatchResult(deliveryOption, resultConsumer, result.left)
-            notifyDataChanged(result.right)
+            val counter = result.right
+            notifyDataChanged(counter.first, counter.second, counter.third)
         }
     }
 
@@ -358,9 +359,12 @@ class ModelServices(
     /**
      * Do always post, never direct. Model changes always need to be handled in GUI thread.
      */
-    private fun notifyDataChanged(changedItems: Int) {
-        if (changedItems > 0) {
-            if (!resultHandler.post { Model.notifyDataChanged(context) }) {
+    private fun notifyDataChanged(changedLists: Int, changedTasks: Int, changedSubtasks: Int) {
+        if (changedLists > 0 || changedTasks > 0 || changedSubtasks > 0) {
+            val success = resultHandler.post {
+                Model.notifyDataChanged(context, changedLists, changedTasks, changedSubtasks)
+            }
+            if (!success) {
                 Log.e(TAG, "Failed to post Model.notifyDataChanged().")
             }
         }

app/src/main/java/org/secuso/privacyfriendlytodolist/model/impl/ModelServicesImpl.kt
@@ -89,22 +89,25 @@ class ModelServicesImpl(private val context: Context) {
         return data as MutableList<TodoTask>
     }
 
-    suspend fun deleteTodoList(todoListId: Int): Int {
+    suspend fun deleteTodoList(todoListId: Int): Triple<Int, Int, Int> {
         var counterLists = 0
         var counterTasks = 0
+        var counterSubtasks = 0
         val todoListData = db.getTodoListDao().getById(todoListId)
         if (null != todoListData) {
             val todoList = loadListsTasksSubtasks(todoListData)[0]
             for (task in todoList.getTasks()) {
-                counterTasks += setTaskAndSubtasksInRecycleBin(task, true)
+                val counter = setTaskAndSubtasksInRecycleBin(task, true)
+                counterTasks += counter.left
+                counterSubtasks += counter.right
             }
-            counterLists = db.getTodoListDao().delete(todoList.data)
+            counterLists += db.getTodoListDao().delete(todoList.data)
         }
-        Log.i(TAG, "$counterLists list with $counterTasks tasks/subtasks removed from database.")
-        return counterLists + counterTasks
+        Log.i(TAG, "$counterLists list with $counterTasks tasks and $counterSubtasks subtasks removed from database.")
+        return Triple(counterLists, counterTasks, counterSubtasks)
     }
 
-    suspend fun deleteTodoTask(todoTask: TodoTask): Int {
+    suspend fun deleteTodoTaskAndSubtasks(todoTask: TodoTask): Tuple<Int, Int> {
         var counterSubtasks = 0
         for (subtask in todoTask.getSubtasks()) {
             counterSubtasks += deleteTodoSubtask(subtask)
@@ -112,7 +115,7 @@ class ModelServicesImpl(private val context: Context) {
         val todoTaskImpl = todoTask as TodoTaskImpl
         val counterTasks = db.getTodoTaskDao().delete(todoTaskImpl.data)
         Log.i(TAG, "$counterTasks task and $counterSubtasks subtasks removed from database.")
-        return counterTasks + counterSubtasks
+        return Tuple(counterTasks, counterSubtasks)
     }
 
     suspend fun deleteTodoSubtask(subtask: TodoSubtask): Int {
@@ -122,7 +125,7 @@ class ModelServicesImpl(private val context: Context) {
         return counter
     }
 
-    suspend fun setTaskAndSubtasksInRecycleBin(todoTask: TodoTask, inRecycleBin: Boolean): Int {
+    suspend fun setTaskAndSubtasksInRecycleBin(todoTask: TodoTask, inRecycleBin: Boolean): Tuple<Int, Int> {
         var counterSubtasks = 0
         for (subtask in todoTask.getSubtasks()) {
             counterSubtasks += setSubtaskInRecycleBin(subtask, inRecycleBin)
@@ -132,7 +135,7 @@ class ModelServicesImpl(private val context: Context) {
         val counterTasks = db.getTodoTaskDao().update(todoTaskImpl.data)
         val action = if (inRecycleBin) "put into" else "restored from"
         Log.i(TAG, "$counterTasks task and $counterSubtasks subtasks $action recycle bin.")
-        return counterTasks + counterSubtasks
+        return Tuple(counterTasks, counterSubtasks)
     }
 
     suspend fun setSubtaskInRecycleBin(subtask: TodoSubtask, inRecycleBin: Boolean): Int {
@@ -171,14 +174,17 @@ class ModelServicesImpl(private val context: Context) {
         return data as MutableList<TodoTask>
     }
 
-    suspend fun clearRecycleBin(): Int {
+    suspend fun clearRecycleBin(): Tuple<Int, Int> {
+        var counterTasks = 0
+        var counterSubtasks = 0
         val dataArray = db.getTodoTaskDao().getAllInRecycleBin()
         val todoTasks = loadTasksSubtasks(true, *dataArray)
-        var counter = 0
         for (todoTask in todoTasks) {
-            counter += deleteTodoTask(todoTask)
+            val counter = deleteTodoTaskAndSubtasks(todoTask)
+            counterTasks += counter.left
+            counterSubtasks += counter.right
         }
-        return counter
+        return Tuple(counterTasks, counterSubtasks)
     }
 
     suspend fun getAllToDoListIds(): MutableList<Int> {
@@ -239,12 +245,13 @@ class ModelServicesImpl(private val context: Context) {
         return counter
     }
 
-    suspend fun saveTodoTaskAndSubtasksInDb(todoTask: TodoTask): Int {
-        var counter = saveTodoTaskInDb(todoTask)
+    suspend fun saveTodoTaskAndSubtasksInDb(todoTask: TodoTask): Tuple<Int, Int> {
+        val counterTasks = saveTodoTaskInDb(todoTask)
+        var counterSubtasks = 0
         for (subtask in todoTask.getSubtasks()) {
-            counter += saveTodoSubtaskInDb(subtask)
+            counterSubtasks += saveTodoSubtaskInDb(subtask)
         }
-        return counter
+        return Tuple(counterTasks, counterSubtasks)
     }
 
     suspend fun saveTodoTaskInDb(todoTask: TodoTask): Int {
@@ -339,11 +346,11 @@ class ModelServicesImpl(private val context: Context) {
         return counter
     }
 
-    suspend fun deleteAllData(): Int {
-        var counter = db.getTodoSubtaskDao().deleteAll()
-        counter += db.getTodoTaskDao().deleteAll()
-        counter += db.getTodoListDao().deleteAll()
-        return counter
+    suspend fun deleteAllData(): Triple<Int, Int, Int> {
+        val counterSubtasks = db.getTodoSubtaskDao().deleteAll()
+        val counterTasks = db.getTodoTaskDao().deleteAll()
+        val counterLists = db.getTodoListDao().deleteAll()
+        return Triple(counterLists, counterTasks, counterSubtasks)
     }
 
     suspend fun exportCSVData(listId: Int?, hasAutoProgress: Boolean, csvDataUri: Uri): String? {
@@ -377,15 +384,15 @@ class ModelServicesImpl(private val context: Context) {
         return null
     }
 
-    suspend fun importCSVData(deleteAllDataBeforeImport: Boolean, csvDataUri: Uri): Tuple<String?, Int> {
+    suspend fun importCSVData(deleteAllDataBeforeImport: Boolean, csvDataUri: Uri): Tuple<String?, Triple<Int, Int, Int>> {
         val inputStream: InputStream?
         try {
             inputStream = context.contentResolver.openInputStream(csvDataUri)
         } catch (e: FileNotFoundException) {
-            return Tuple("Input file not found.", 0)
+            return Tuple("Input file not found.", Triple(0, 0, 0))
         }
         if (null == inputStream) {
-            return Tuple("Failed to open input file.", 0)
+            return Tuple("Failed to open input file.", Triple(0, 0, 0))
         }
         val csvImporter = CSVImporter()
         try {
@@ -393,33 +400,35 @@ class ModelServicesImpl(private val context: Context) {
                 csvImporter.import(reader)
             }
         } catch (e: Exception) {
-            return Tuple(e.message, 0)
+            return Tuple(e.message, Triple(0, 0, 0))
         }
 
-        var counter = 0
         if (deleteAllDataBeforeImport) {
             Log.i(TAG, "Deleting all data due to CSV data import.")
-            counter += deleteAllData()
+            deleteAllData()
         }
 
+        var counterLists = 0
         for (list in csvImporter.lists.values) {
-            counter += saveTodoListInDb(list)
+            counterLists += saveTodoListInDb(list)
         }
+        var counterTasks = 0
         for (task in csvImporter.tasks.values) {
             // The tasks list gets its ID while saving it in DB.
             // So update the list ID at the task after the list was saved in DB.
             if (null != task.left) {
                 task.right.setListId(task.left.getId())
             }
-            counter += saveTodoTaskInDb(task.right)
+            counterTasks += saveTodoTaskInDb(task.right)
         }
+        var counterSubtasks = 0
         for (subtask in csvImporter.subtasks.values) {
             // The subtasks task gets its ID while saving it in DB.
             // So update the task ID at the subtask after the task was saved in DB.
             subtask.right.setTaskId(subtask.left.getId())
-            counter += saveTodoSubtaskInDb(subtask.right)
+            counterSubtasks += saveTodoSubtaskInDb(subtask.right)
         }
-        return Tuple(null, counter)
+        return Tuple(null, Triple(counterLists, counterTasks, counterSubtasks))
     }
 
     private suspend fun loadListsTasksSubtasks(vararg dataArray: TodoListData): MutableList<TodoListImpl> {

app/src/main/java/org/secuso/privacyfriendlytodolist/observer/TaskChangeObserver.kt
@@ -1,0 +1,42 @@
+/*
+Privacy Friendly To-Do List
+Copyright (C) 2024  Christian Adams
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+package org.secuso.privacyfriendlytodolist.observer
+
+import android.content.Context
+import android.util.Log
+import org.secuso.privacyfriendlytodolist.model.ModelObserver
+import org.secuso.privacyfriendlytodolist.service.JobManager
+import org.secuso.privacyfriendlytodolist.util.LogTag
+
+object TaskChangeObserver: ModelObserver {
+    private val TAG = LogTag.create(this::class.java)
+
+    override fun onTodoDataChanged(context: Context, changedLists: Int, changedTasks: Int, changedSubtasks: Int) {
+        if (0 != changedTasks) {
+            Log.d(TAG, "$changedTasks tasks did change. Updating alarm job.")
+            JobManager.startUpdateAlarmJob(context)
+        }
+    }
+
+    override fun onTodoDataChangedFromOutside(context: Context, changedLists: Int, changedTasks: Int, changedSubtasks: Int) {
+        if (0 != changedTasks) {
+            Log.d(TAG, "$changedTasks tasks did change from outside. Updating alarm job.")
+            JobManager.startUpdateAlarmJob(context)
+        }
+    }
+}

app/src/main/java/org/secuso/privacyfriendlytodolist/service/HandleAlarmJob.kt
@@ -29,7 +29,7 @@ import org.secuso.privacyfriendlytodolist.util.NotificationMgr
 /**
  * A job service that handles an alarm which means showing a reminder notification.
  */
-class HandleAlarmJob : ModelJobBase("Handle-alarm job") {
+class HandleAlarmJob : ModelJobBase("Handle-alarm-job") {
     override fun onStartJob(): Boolean {
         super.onStartJob()
 
@@ -108,7 +108,7 @@ class HandleAlarmJob : ModelJobBase("Handle-alarm job") {
 
             if (counter > 0) {
                 Log.i(TAG, "$logPrefix Set $task automatically as undone because its re-occurring soon.")
-                Model.notifyDataChangedFromOutside(context)
+                Model.notifyDataChangedFromOutside(context, 0, counter, 0)
             } else {
                 Log.e(TAG, "$logPrefix Failed to set $task as undone. Result: $counter")
             }

commit 8d9ff0db803b673b674c91e157125f6eb3f3cccd

Fixed issue #155: Task done-status did not depend on subtasks-done-status. Commit 8b48ed709723648f036225fe05481e0200f605cd accidentally did remove this feature.

app/src/main/java/org/secuso/privacyfriendlytodolist/model/TodoTask.kt
@@ -142,12 +142,12 @@ interface TodoTask : BaseTodo, Parcelable {
     fun getDoneTime(): Long?
 
     /**
-     * A task is done if the user manually sets it done or when all subtasks are done.
-     * If a subtask is selected "done", the entire task might be "done" if by now all subtasks are done.
-     * This method checks if done-status of task needs an update due to this and if so, does the update.
+     * This method updates the done-status of the task depending on the done-status of all subtasks:
+     * - If all subtasks are done and the task is not set as done, it gets set as done.
+     * - If not all subtasks are done and the task is set as done, it gets set as undone.
      * @return true if the done-status was updated, otherwise false.
      */
-    fun doneStatusChanged(): Boolean
+    fun updateDoneStatus(): Boolean
     fun setInRecycleBin(isInRecycleBin: Boolean)
     fun isInRecycleBin(): Boolean
     fun checkQueryMatch(query: String?, recursive: Boolean): Boolean

app/src/main/java/org/secuso/privacyfriendlytodolist/model/impl/TodoTaskImpl.kt
@@ -308,9 +308,7 @@ class TodoTaskImpl : BaseTodoImpl, TodoTask {
         return data.doneTime
     }
 
-    // A task is done if the user manually sets it done or when all subtasks are done.
-    // If a subtask is selected "done", the entire task might be "done" if by now all subtasks are done.
-    override fun doneStatusChanged(): Boolean {
+    override fun updateDoneStatus(): Boolean {
         var allSubtasksAreDone = true
         for (subtask in subtasks) {
             if (!subtask.isDone()) {

app/src/main/java/org/secuso/privacyfriendlytodolist/view/ExpandableTodoTaskAdapter.kt
@@ -574,10 +574,14 @@ class ExpandableTodoTaskAdapter(private val context: Context, private val model:
                     if (buttonView.isPressed) {
                         currentSubtask.setDone(buttonView.isChecked)
                         currentSubtask.setChanged()
+                        val doneStatusChanged = currentTask.updateDoneStatus()
                         model.saveTodoSubtaskInDb(currentSubtask) {
-                            if (hasAutoProgress()) {
+                            val hasAutoProgress = hasAutoProgress()
+                            if (hasAutoProgress) {
                                 // If having auto-progress, update the progress and save it.
                                 currentTask.getProgress(true)
+                            }
+                            if (doneStatusChanged || hasAutoProgress) {
                                 model.saveTodoTaskInDb(currentTask) {
                                     notifyDataSetChanged()
                                 }